3a7448994d645c1d3545534a1464ff6d3f1408b2
Yohannes Kristianto <yohannes.kristianto218@gmail.com>
2022-05-31 22:54:00 +0700

    ui mockup

--- web/src/page/Root.svelte
+++ web/src/page/Root.svelte
@@ -7,14 +7,48 @@
     }
 </script>
 
-Root
 
+<hr/>
+<div class="box">+</div>
 {#await API.GetRoot()}
 	<p>...loading</p>
 {:then res}
     {#each res.roots as v, i}
-	<a href="#/cert/{i}" on:click|preventDefault={() => navigate("#/cert/"+i)}><Cert name={v.name} /></a>
+        <div class="box-cert">
+            <a href="#/cert/{i}" on:click|preventDefault={() => navigate("#/cert/"+i)}>
+                <Cert name={v.name}/>
+            </a>
+        </div>
     {/each}
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
+
+<style>
+    .box-cert {
+        margin: 5px; display: inline-block;
+    }
+    .box-cert:hover {
+        box-shadow: 2px 2px 2px #aaa;
+    }
+    .box-cert a {text-decoration: none;}
+    .box {
+        margin: 5px;
+        margin-top: 20px;
+        display: inline-block;
+        vertical-align: top;
+        width: 100px;
+        height: 75px;
+        background: #aaa;
+        border-radius: 100%;
+        text-align: center;
+        padding-top: 25px;
+        font-size: 24pt;
+        cursor: pointer;
+        transition: all .2s linear;
+    }
+    .box:hover {
+        background: #eee;
+        box-shadow: 0px 0px 5px #aaa;
+    }
+</style>
